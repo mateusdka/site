ce35438a047b9679c3db0f44246f78840f0b84d3
mateusdka <mateusdka@gmail.com>
2025-04-06 17:37:16 -0300

    Ajusts finais para Build

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <div>
@@ -12,21 +14,21 @@ function Home() {
         <div className="col-lg bg-light rounded p-4">
           <h5 className="banner-text rounded p-3">Calculadora de Campanhas</h5>
           <p>Simule de forma rápida e intuitiva a distribuição de investimento em campanhas digitais. Descubra como alcançar o público certo e gerar resultados com base em indicadores reais do marketing digital.</p>
-          <p><a href="/projetos/calculadora-de-campanhas" className="btn btn-outline-dark">Acessar projeto</a></p>
+          <Link to="/projetos/calculadora-de-campanhas" className="btn btn-outline-dark">Acessar Calculadora</Link>
         </div>
 
         {/* Bloco - Cartas de Tarô */}
         <div className="col-lg bg-light rounded p-4">
           <h5 className="banner-text rounded p-3">Cartas de Tarô</h5>
           <p>Explore os significados simbólicos das cartas de Tarô com visualização interativa, sorteio aleatório e representação de cartas invertidas. Uma jornada de autoconhecimento com React e dados dinâmicos.</p>
-          <p><a href="/projetos/cartas-de-tarot" className="btn btn-outline-dark">Explorar cartas</a></p>
+          <Link to="/projetos/cartas-de-tarot" className="btn btn-outline-dark">Explorar Cartas</Link>
         </div>
 
         {/* Bloco - Sobre Mim */}
         <div className="col-lg bg-light rounded p-4">
           <h5 className="banner-text rounded p-3">Sobre Mim</h5>
           <p>Mais do que demonstrar habilidades técnicas, este espaço é uma forma de compartilhar a jornada de quem está começando na área com dedicação e curiosidade.</p>
-          <p><a href="/sobre" className="btn btn-outline-dark">Ver mais</a></p>
+          <Link to="/sobre" className="btn btn-outline-dark">Saiba Mais</Link>          
         </div>
 
 
