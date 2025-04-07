78b553089c8c287747c812c33301ebbe8b097eba
mateusdka <mateusdka@gmail.com>
2025-04-06 21:49:48 -0300

    Ajustes estéticos finais no Menu

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,17 +1,31 @@
 // Menu de navegação do site
 // Este componente é responsável por renderizar o menu de navegação do site.
 // Ele utiliza o React Router para gerenciar as rotas e a navegação entre as páginas.
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import Logo from "../Img/logo.svg";
+import LogoWhite from '../Img/logowhite.svg';
 
 function Navigation() {
+  const [scrolled, setScrolled] = useState(false); // Controla estado da rolagem para modificar o menu
   const location = useLocation(); // Hook do React Router para obter a localização atual
 
+  useEffect(() => { // Modifica o estado da rolagem
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+  
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  
+  
   return (
-    <nav className="navbar navbar-expand-lg bg-white shadow-sm">
+    <nav id="mainNavbar" className={`navbar navbar-expand-lg fixed-top shadow-sm ${scrolled ? "bg-dark navbar-dark" : "bg-white navbar-light"}`}>
+      {/* O menu responde ao efeito da rolagem de tela para mudar de classes */}
+      {/* O logo também se inverte, conforme o estado da rolagem muda */}
       <div className="container">
-        <a className="navbar-brand" href="/"><img className="me-4" src={Logo} width={100} /> mateus fardin</a>
+        <a className="navbar-brand" href="/"><img className="me-4" src={ scrolled ? LogoWhite : Logo } width={100} alt="Logo Mateus Fardin" /> mateus fardin</a>
 
         {/* Botão para alternar o menu em telas menores */}
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
@@ -28,7 +42,7 @@ function Navigation() {
               <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Projetos
               </a>
-              <ul className="dropdown-menu"> {/* Submenu de projetos */}
+              <ul className={`dropdown-menu ${scrolled ? "bg-dark navbar-dark" : ""}`}> {/* Submenu de projetos */}
                 <li>
                   <Link to="/projetos/calculadora-de-campanhas" className={location.pathname === '/projetos/calculadora-de-campanhas' ?
                     'nav-link nav-active' : 'nav-link'}>Calculadora de Campanhas
