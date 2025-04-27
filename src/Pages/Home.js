010cb25289f68e6e07d308c8a96417b1ba6bff8c
mateusdka <mateusdka@gmail.com>
2025-04-26 23:44:41 -0300

    Home Update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
       </div>
 
       <div className="container-lg bg-white rounded p-5 mt-5">
-        <div className="row gap-4 text-center">
+        <div className="row gap-2 text-center">
 
         {/* Bloco - Calculadora de Campanhas */}
         <div className="col-lg bg-light rounded p-4">
@@ -32,20 +32,11 @@ function Home() {
         </div>
 
         {/* Bloco - Preços Agropecuários */}
-        <div className="col-lg bg-light rounded p-4">
+        <div className="col-lg-3 bg-light rounded p-4">
           <h5 className="banner-text rounded p-3">Preços Agropecuários</h5>
           <p>Acompanhe a evolução dos preços médios de produtos agropecuários com gráficos interativos e exportação de dados. Ideal para análises de mercado e estudos de correlação.</p>
           <Link to="/projetos/precos" className="btn btn-outline-dark">Visualizar Preços</Link>
         </div>
-
-        {/* Bloco - Sobre Mim */}
-        <div className="col-lg bg-light rounded p-4">
-          <h5 className="banner-text rounded p-3">Sobre Mim</h5>
-          <p>Mais do que demonstrar habilidades técnicas, este espaço é uma forma de compartilhar a jornada de quem está começando na área com dedicação e curiosidade.</p>
-          <Link to="/sobre" className="btn btn-outline-dark">Saiba Mais</Link>          
-        </div>
-
-
       </div>
     </div>
 </div>
